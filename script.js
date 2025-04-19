9b1a195b42a876cfd7ddd887e1f5183ff7ce73bd
rustyladx <you@example.com>
2025-04-20 00:51:35 +0600

    Upgrade website with new design and features

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  new Typed('#typed-text', {
+    strings: ["Developer.", "Hacker.", "Creator."],
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true
+  });
+});
